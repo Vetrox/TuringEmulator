e15636269f55e7a6dde624eeda8142fe67491597
Vetrox <39677514+Vetrox@users.noreply.github.com>
2019-12-07 19:31:59 +0100

    unordered_map

--- TuringEmulator/TuringEmulator.cpp
+++ TuringEmulator/TuringEmulator.cpp
@@ -1,81 +1,55 @@
-// TuringEmulator.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <string>
 #include <chrono>
+#include <unordered_map>
+#include <tuple>
+
 
 typedef unsigned char uch;
 typedef long long int lli;
 using namespace std;
 
-uch start;
-uch ende;
+uch start, ende;
+vector<uch> band;
 
 
-struct Transition {
-	uch zustand;
-	uch element;
-
-	uch zustand_neu;
-	uch push_element;
-	uch instruction;
-};
-
-void daten_einlesen(vector<Transition>& transitions, vector<uch>& band, int argc, char** argv) {
-	vector<string> data;
-	string line;
-	string path = "";
+void daten_einlesen(unordered_map<uch, unordered_map<uch, tuple<uch, uch, uch>>>& transitions, vector<uch>& band, int argc, char** argv) {
+	string line, path;
 	if (argc >= 2) {
 		path = string(argv[1]);
 		cout << "Dateiname aus Argumenten: " << path << endl;
-	}
-	else {
+	} else {
 		cout << "Dateinamen eingeben: ";
 		cin >> path;
 	}
-	
-	
-	
+
+	vector<string> data;
 	ifstream myfile(path);
-	if (myfile.is_open())
-	{
+	if (myfile.is_open()) {
 		while (getline(myfile, line))
-		{
 			data.push_back(line);
-		}
 		myfile.close();
 	}
+
 	start = data[0][6];
 	ende = data[1][5];
 	string b = data[2].substr(5);
-	for (char c : b) {
+	for (char c : b)
 		band.push_back(c);
-	}
 
-	for (uch i = 3; i < data.size(); i++) {
-		string d = data[i];
-		Transition t;
-		t.zustand = d[0];
-		t.element = d[2];
-		t.zustand_neu = d[7];
-		t.push_element = d[9];
-		t.instruction = d[11];
-		transitions.push_back(t);
-	}
+	for (uch i = 3; i < data.size(); i++)
+		transitions[data[i][0]][data[i][2]] = tuple<uch, uch, uch>(data[i][7], data[i][9], data[i][11]);
 }
 
-lli read(vector<uch>& band, const lli& head, lli& vectorMid, uch zustand) {
+uch& read(vector<uch>& band, const lli& head, lli& vectorMid, uch zustand) {
 	if (head + vectorMid < 0) {
 		band.insert(band.begin(), '_');
 		vectorMid++;
-	}
-	else if (head + vectorMid >= band.size()) {
+	} else if (head + vectorMid >= band.size()) 
 		band.push_back('_');
-	}
-	return vectorMid + head;
+	return band[vectorMid + head];
 }
 
 void parseInstruction(const uch& instruction, lli& head) {
@@ -85,85 +59,44 @@ void parseInstruction(const uch& instruction, lli& head) {
 
 int main(int argc, char** argv) {
 	//the setup
-	vector<uch> band;
-	vector<Transition> transitions;
+	unordered_map<uch, unordered_map<uch, tuple<uch, uch, uch>>> transitions;
 	daten_einlesen(transitions, band, argc, argv);
 
 	cout << "Bedingungen" << endl;
 	cout << "Start = " << start << " Ende = " << ende << endl;
-	for (Transition t : transitions)
-		cout << t.zustand << " " << t.element << " -> " << t.zustand_neu << " " << t.push_element << " " << t.instruction << endl;
-
-
-	cout << "Band: ";
-	for (uch c : band)
-		cout << c << " ";
-	cout << endl;
-
 
 	//setting up the turing computation
-	lli head = 0;
-	lli vectorMid = 0;
-	lli counter = 0;
+	lli head = 0, vectorMid = 0, counter = 0;
 	uch zustand = start;
 
-	lli prev_head = head;
-	lli prev_prev_head = prev_head;
-	uch prev_zustand = start;
-	bool right = true;
-
-
 	//time before calculation
-	chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-	chrono::steady_clock::time_point end = chrono::steady_clock::now();
+	auto begin = chrono::steady_clock::now();
 
 	//the turing simulation
 	while (zustand != ende) {
-		uch& container = band[read(band, head, vectorMid, zustand)];
-		
-		
-		for (Transition trans : transitions) {
-			if (trans.zustand == zustand and trans.element == container) {
-				zustand = trans.zustand_neu;
-				container = trans.push_element;
-				parseInstruction(trans.instruction, head);
-				break;
-			}
-		}
-
-		if (head == prev_prev_head) {
-			if (right == true) {
-				cout << "	" << prev_zustand << endl;
-			}
-			else {
-				cout << prev_zustand << endl;
-			}
-			right = !right;
-		}
-		prev_prev_head = prev_head;
-		prev_head = head;
-		prev_zustand = zustand;
+		uch& container = read(band, head, vectorMid, zustand);
+		auto c = transitions[zustand][container];
+		zustand = get<0>(c);
+		container = get<1>(c);
+		parseInstruction(get<2>(c), head);
 		counter++;
-
-		if (chrono::duration_cast<chrono::seconds> (chrono::steady_clock::now() - end).count() > 10) {
-			cout << "Operationen: " << counter << endl;
-			end = chrono::steady_clock::now();
-		}
 	}
-	end = chrono::steady_clock::now();
-	lli elapsed_secs = chrono::duration_cast<chrono::seconds> (end - begin).count();
+	auto end = chrono::steady_clock::now();
 
+
+	//berechnungen machen
 	lli anz_zeichen = 0;
 	for (uch c : band)
 		if (c != '_') anz_zeichen++;
+	lli elapsed_ms = chrono::duration_cast<chrono::milliseconds> (end - begin).count();
 
 
-	cout << "Halt-Status nach " << elapsed_secs << "s erreicht." << counter << " Operationen, " << anz_zeichen << " Zeichen auf dem Band." << endl;
+	//ausgabe an den user
+	cout << "Halt-Status nach " << elapsed_ms << "ms erreicht." << counter << " Operationen, " << anz_zeichen << " Zeichen auf dem Band." << endl;
 	for (uch c : band) {
 		cout << c << " ";
 	}
 	cout << endl;
-
 
 	system("pause");
 }
